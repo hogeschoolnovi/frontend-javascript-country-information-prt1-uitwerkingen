e78e4fbcd5e4656e23c666f40295da6a21c0a3f7
novaeeken <nova_eeken@hotmail.com>
2021-10-29 11:10:19 +0200

    Set up assignment

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+// if (module.hot) {
+//   module.hot.accept(function () {
+//     location.reload();
+//   });
+// }
+//
+// async function fetchData() {
+//   try {
+//     const result = await axios.get('https://restcountries.com/v3.1/all');
+//     console.log(result.data);
+//   } catch (e) {
+//     console.error(e);
+//   }
+// }
+//
+// fetchData();
+//
+// console.log('hallo');
